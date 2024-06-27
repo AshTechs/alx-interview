c0e4524738dc175c7f73dcdf59474a6f59922fec
AshTechs <ashbelh@gmail.com>
2024-06-26 21:23:54 -0700

    Corrected

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,55 +1,43 @@
-#!/usr/bin/python3
-"""
-Defines a class PascalTriangle to generate Pascal's Triangle up to n rows.
-"""
+def pascal_triangle(n):
+    """
+    Generates Pascal's Triangle up to n rows.
 
-class PascalTriangle:
-    """Represents Pascal's Triangle up to n rows."""
+    Args:
+        n (int): Number of rows in the triangle.
 
-    def __init__(self, n):
-        """Initializes Pascal's Triangle with up to n rows.
+    Returns:
+        list of lists: Pascal's Triangle as a list of lists of integers.
+    """
+    triangle = []
 
-        Args:
-            n (int): Number of rows to generate.
-        """
-        self.n = n
-
-    @property
-    def n(self):
-        """Gets/Sets the number of rows in Pascal's Triangle."""
-        return self.__n
-
-    @n.setter
-    def n(self, value):
-        if not isinstance(value, int):
-            raise TypeError("n must be an integer")
-        elif value <= 0:
-            raise ValueError("n must be greater than 0")
-        self.__n = value
-
-    def generate_triangle(self):
-        """Generates Pascal's Triangle up to n rows."""
-        triangle = []
-
-        for i in range(self.n):
-            row = [1] * (i + 1)
-            for j in range(1, i):
-                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-            triangle.append(row)
-
+    if n <= 0:
         return triangle
 
-    def print_triangle(self):
-        """Prints Pascal's Triangle."""
-        triangle = self.generate_triangle()
-        for row in triangle:
-            print(" ".join(map(str, row)).center(self.n * 3))
+    # Generate first row
+    triangle.append([1])
+
+    for i in range(1, n):
+        prev_row = triangle[i - 1]
+        current_row = [1]
+
+        for j in range(1, i):
+            current_row.append(prev_row[j - 1] + prev_row[j])
+
+        current_row.append(1)
+        triangle.append(current_row)
+
+    return triangle
 
 # Example usage:
 if __name__ == "__main__":
-    try:
-        n = int(input("Enter the number of rows for Pascal's Triangle: "))
-        pt = PascalTriangle(n)
-        pt.print_triangle()
-    except ValueError as e:
-        print(f"Error: {e}. Please enter a valid integer greater than 0.")
+    def print_triangle(triangle):
+        """
+        Prints the Pascal's Triangle.
+
+        Args:
+            triangle (list of lists): Pascal's Triangle represented as a list of lists of integers.
+        """
+        for row in triangle:
+            print("[{}]".format(",".join([str(x) for x in row])))
+
+    print_triangle(pascal_triangle(5))
