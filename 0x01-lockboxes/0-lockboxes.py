bac03df3f94c0ffa89c995431030106998872c4b
AshTechs <ashbelh@gmail.com>
2024-07-02 20:50:19 -0700

    Done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -15,29 +15,14 @@ def canUnlockAll(boxes):
         bool: True if all boxes can be opened, False otherwise.
     """
     n = len(boxes)
-    if n == 0:
-        return False
+    opened = set([0])
+    stack = [0]
 
-    visited = [False] * n
-    visited[0] = True
-    queue = [0]
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if key not in opened and key < n:
+                opened.add(key)
+                stack.append(key)
 
-    while queue:
-        current = queue.pop(0)
-        for key in boxes[current]:
-            if not visited[key]:
-                visited[key] = True
-                queue.append(key)
-
-    return all(visited)
-
-
-if __name__ == "__main__":
-    boxes1 = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes1))
-
-    boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes2))
-
-    boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes3))
+    return len(opened) == n
