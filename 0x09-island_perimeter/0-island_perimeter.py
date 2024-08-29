6aa1047c35e78e865804c7f96373cc80b1913ac6
AshTechs <ashbelh@gmail.com>
2024-08-29 13:32:10 +0000

    Done

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
+"""
+This module contains the function island_perimeter.
+"""
+
+
 def island_perimeter(grid):
     """
-    Calculate the perimeter of an island in a grid.
+    Calculates the perimeter of the island described in grid.
 
     Args:
-        grid: 2D list of integers representing the map, where 1 is land and 0 is water.
+        grid (list of list of int): A 2D list representing the map.
 
     Returns:
         int: The perimeter of the island.
     """
-    if not grid or not grid[0]:
-        return 0
-
     perimeter = 0
     rows = len(grid)
     cols = len(grid[0])
 
-    for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == 1:
-                if r == 0 or grid[r-1][c] == 0:
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                if r == rows-1 or grid[r+1][c] == 0:
+                if i == rows - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
-                if c == 0 or grid[r][c-1] == 0:
+                if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                if c == cols-1 or grid[r][c+1] == 0:
+                if j == cols - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
 
     return perimeter
